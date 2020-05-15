075831d972830c4b71494454128a2b035fea5453
kaxevgg <hi@arpitbansal.com>
2020-05-15 22:12:56 +0800

    Removing mock sum.js and associated test file

--- __tests__/sum.test.js
+++
@@ -1,5 +0,0 @@
-import { sum, minus } from '../sum';
-
-test('sum', () => {
-    expect(sum(1,2)).toBe(3)
-});
--- sum.js
+++
@@ -1,7 +0,0 @@
-export function sum(num1, num2) {
-    return num1 + num2;
-}
-
-export function minus(num1, num2) {
-    return num1 - num2;
-}
