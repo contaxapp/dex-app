51806bd4f92f33244d23d4857bd3bd0f7a65fa8a
kaxevgg <hi@arpitbansal.com>
2020-05-17 13:20:31 +0800

    Added basic rendering functionality for fetched contacts
    1. Added ContactItem.js functional component to display contact info
    2. Added onPress method to console.log contact details when pressed

---
+++ src/screens/Contact.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet
+} from 'react-native';
+
+import { getContacts } from '../library/utils/contact';
+import ContactItem from '../library/components/ContactItem';
+
+class Contact extends Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+      contacts: []
+    }
+
+    this.printContacts = this.printContacts.bind(this);
+  }
+
+  componentDidMount() {
+    getContacts((contacts) => {
+      console.log(contacts);
+      this.setState({ contacts })
+    });
+  }
+
+  printContacts(contacts) {
+    if (contacts.length) {
+      return (
+        <View>
+          {contacts.map((contact, key) => {
+            return <ContactItem key={key} contact={contact} />
+          })}
+        </View>
+      );
+    } else {
+      return <Text>No contacts found</Text>
+    }
+  }
+
+  render() {
+    return (
+      <View style={[styles.container]}>
+          <Text>Contacts Home Screen</Text>
+          {this.printContacts(this.state.contacts)}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
+
+export default Contact;
