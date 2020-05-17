b9ac172769c12159fd61e78b52dfc933bfe3eddc
kaxevgg <hi@arpitbansal.com>
2020-05-17 13:20:02 +0800

    Changed src project structure
    1. Added library/ to manage all shares resources
    2. Added library/components to manage shared components
    3. Added library/utils to manage shared helper functions
    4. Added library/networking to manage all networking operations
    5. Renamed components/ to screens/ to manage all main screens

--- __tests__/Home.test.js
+++ __tests__/Home.test.js
@@ -1,6 +1,6 @@
 import 'react-native';
 import React from 'react';
-import Home from '../src/components/Home';
+import Home from '../src/screens/Home';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
